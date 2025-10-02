e01a9ced60c35fc20eb3264f45e86387bfeb2939
kapustnyak <zned003@gmail.com>
2025-10-02 20:28:17 +0200

    Header and hero are finished

---
+++ src/app/HomePage.tsx
@@ -0,0 +1,11 @@
+import hp from './HomePage.module.css'
+import Hero from "../components/Hero/Hero"
+
+export default function HomePage () {
+    return (
+    <div className={hp.container}>
+        <Hero />
+    </div>
+
+)
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "modern-normalize";
-import App from "./components/App/App.tsx";
+import 'modern-normalize';
+import App from "./app/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
